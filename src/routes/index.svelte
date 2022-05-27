0b10fa221f742b854eb521594cb3e5e3db82b1fc
Blit Wagon <blitwagon@gmail.com>
2022-05-26 21:45:31 -0500

    add links

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,12 +4,29 @@
 <main class="markup column">
 	<section>
 		<h1>Blit Wagon</h1>
-		<p class="caption">open source web hobby-grade software and tools</p>
+		<p class="caption">open source web hobby-grade software & tools</p>
+		<p>
+			<a href="https://flat.cosmicplayground.org">flat.cosmicplayground.org</a> is a forkable educational
+			game and creative environment featuring open source hobby-grade web tools for playing with 2d things
+			in Svelte, SvelteKit, TypeScript, and PixiJS
+		</p>
 		<p>
 			<a href="https://www.cosmicplayground.org">cosmicplayground.org</a> open source web site
 		</p>
 		<p>
-			<a href="https://www.cosmicplayground.dev">cosmicplayground.dev</a> community+more (coming soon)
+			<a href="https://www.cosmicplayground.dev">cosmicplayground.dev</a> community+more, see the
+			<a href="https://github.com/cosmicplayground/community/blob/main/code_of_conduct.md"
+				>code of conduct</a
+			>,
+			<a href="https://github.com/cosmicplayground/community/blob/main/rules_and_norms.md"
+				>rules and norms</a
+			>,
+			<a href="https://github.com/cosmicplayground/community/blob/main/contributing.md"
+				>contributing</a
+			>, <a href="https://github.com/cosmicplayground/community/blob/main/privacy.md">privacy</a>,
+			and
+			<a href="https://github.com/cosmicplayground/community/blob/main/governance.md">governance</a>
+			docs
 		</p>
 		<p>
 			@blitwagon on <a href="https://twitter.com/blitwagon">Twitter</a> and
