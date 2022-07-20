80bba27c094b2434992125c21ee646998b378b46
Blit Wagon <blitwagon@gmail.com>
2022-07-19 19:51:14 -0500

    rearrange

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,12 +6,12 @@
 		<h1>Blit Wagon</h1>
 		<p class="caption">open source web hobby-grade software & tools</p>
 		<p>
-			<a href="https://flat.cosmicplayground.org">flat.cosmicplayground.org</a> is a forkable game and
-			creative educational environment featuring open source hobby-grade web tools for playing with 2d
-			things in Svelte, SvelteKit, TypeScript, and PixiJS
+			<a href="https://www.cosmicplayground.org">cosmicplayground.org</a> open source web site
 		</p>
 		<p>
-			<a href="https://www.cosmicplayground.org">cosmicplayground.org</a> open source web site
+			<a href="https://flat.cosmicplayground.org">flat.cosmicplayground.org</a> is a
+			<em>[work in progress]</em> forkable game and creative educational environment featuring open source
+			hobby-grade web tools for playing with 2d things in Svelte, SvelteKit, TypeScript, and PixiJS
 		</p>
 		<p>
 			<a href="https://www.cosmicplayground.dev">cosmicplayground.dev</a> community+more, see the
